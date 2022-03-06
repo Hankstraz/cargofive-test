e89cb1578a776fd012b79949c09217067d88e6d7
Sergio Lugo <33262738+Hankstraz@users.noreply.github.com>
2022-03-06 10:05:57 -0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <img alt="Cargofive logo" src="./assets/cargologo.jpg">
+  
+
+  <div class="container mt-4">
+    <JqueryTable/>
+  </div>
+</template>
+
+<script>
+import JqueryTable from './components/jqueryTable.vue'
+
+export default {
+  name: 'App',
+  components: {
+    
+    JqueryTable
+  }
+}
+</script>
+
+<style>
+.container {
+    max-width: 1000px;
+  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/jqueryTable.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+      <h2>Tabla de Puertos</h2>
+      <table class="table" id="datatable">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Pais</th>
+            <th>Continente</th>
+            <th>Coordenadas</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in puertos" :key="item.id">
+            <td>{{item.id}}</td>
+            <td>{{item.name}}</td>
+            <td>{{item.country}}</td>
+            <td>{{item.continent}}</td>
+            <td>{{item.coordinates}}</td>
+          </tr>
+          
+        </tbody>
+      </table>    
+  </div>
+</template>
+ 
+<script>
+import 'jquery/dist/jquery.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "datatables.net-dt/js/dataTables.dataTables"
+import "datatables.net-dt/css/jquery.dataTables.min.css"
+import axios from 'axios';
+import $ from 'jquery'; 
+
+export default {
+  mounted(){
+    axios.get("http://apitest.cargofive.com/api/ports").then((response)=>
+    {
+      this.puertos = response.data;
+      $('#datatable').DataTable();
+    })
+  },
+  data: function() {
+        return {
+            puertos:[]
+        }
+    },
+}
+</script>
